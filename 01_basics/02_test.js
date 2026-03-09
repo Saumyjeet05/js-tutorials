353f0130fdabc5f529bb86c71f4c8dce14968399
Saumyjeet05 <psaumyjeet07@gmail.com>
2026-03-09 06:08:23 +0000

    note added

--- 01_basics/02_test.js
+++ 01_basics/02_test.js
@@ -2,7 +2,7 @@ const accountId = 3456
 let accountEmail="hello@gmail"
 var accountPassword ="8787"
 accountCity = "gkp"
-
+// never use var for variable declaration because of its scope issue and hoisting issue and also it can be redeclared and updated but let and const cannot be redeclared but can be updated except const which cannot be updated as well
 console.log(accountId);
 console.table([accountEmail,accountPassword,accountId,accountCity]);
 
