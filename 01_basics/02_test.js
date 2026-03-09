219ea363c49607fb4b1d0acc442885fdeaa21e2b
Saumyjeet05 <psaumyjeet07@gmail.com>
2026-03-09 06:02:13 +0000

    dev re config and add test file

---
+++ 01_basics/02_test.js
@@ -0,0 +1,9 @@
+const accountId = 3456
+let accountEmail="hello@gmail"
+var accountPassword ="8787"
+accountCity = "gkp"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountId,accountCity]);
+
+
